ad6de30b661aa7fa484b57fcb41fcde13dd2619f
agnivon <agnivon@gmail.com>
2023-09-26 20:53:43 +0530

    V1.0.0

---
+++ app.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/api", (req: Request, res: Response) => {
+  const date = new Date();
+  return res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+});
+
+app.get("/api/:time", (req: Request, res: Response) => {
+  const date = new Date(Number(req.params.time) || req.params.time);
+  if (isNaN(date.valueOf())) return res.json({ error: "Invalid Date" });
+  return res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
